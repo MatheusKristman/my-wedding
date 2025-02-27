a23146a20557e61ad4293141630a052ef12dd11c
MatheusKristman <kristman058@gmail.com>
2025-02-27 16:49:30 -0300

    added responsive modal

--- app/(website)/lista-de-presentes/components/cart-dialog.tsx
+++ app/(website)/lista-de-presentes/components/cart-dialog.tsx
@@ -10,7 +10,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 
 import { trpc } from "@/lib/trpc-client";
-import { formatPrice } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
 
 interface CartDialogProps {
   width: number | null;
@@ -157,7 +157,9 @@ export function CartDialog({ width, openCart, giftsSelected, setGiftsSelected, s
         </Button>
       </DialogTrigger>
 
-      <DialogContent className="sm:rounded-none">
+      <DialogContent
+        className={cn("sm:rounded-none !max-w-3xl", { "!max-w-xl": !data || !totalPrice || data.length === 0 })}
+      >
         <DialogHeader>
           <DialogTitle className="uppercase font-light text-2xl font-montserrat">Seu carrinho</DialogTitle>
         </DialogHeader>
@@ -171,46 +173,46 @@ export function CartDialog({ width, openCart, giftsSelected, setGiftsSelected, s
             </Button>
           </div>
         ) : data && totalPrice && data.length > 0 ? (
-          <div className="w-full mt-12 flex flex-col items-center justify-center gap-12">
-            <ScrollArea className="max-h-[370px] h-full w-full">
-              <div className="w-full h-full flex flex-col gap-4">
-                {data.map((gift) => (
-                  <div key={gift.id} className="w-full flex">
-                    <div className="w-1/3 shrink-0 aspect-square relative">
-                      <Image src={gift.imageUrl} alt={gift.name} fill className="object-cover object-center" />
-                    </div>
-
-                    <div className="w-full p-6 bg-secondary flex flex-col justify-between gap-6">
-                      <div className="w-full flex flex-col gap-2">
-                        <div className="w-full flex items-center gap-2">
-                          <div className="w-8 h-px bg-background" />
-
-                          <h4 className="text-xl font-montserrat font-normal text-background uppercase line-clamp-1">
-                            {gift.name}
-                          </h4>
-                        </div>
-
-                        <h2 className="text-4xl font-montserrat font-normal text-background">
-                          {formatPrice(gift.price / 100)}
-                        </h2>
+          <div className="w-full mt-12 flex items-center justify-center gap-12">
+            <div className="w-1/2 h-full flex flex-col justify-between gap-12">
+              <ScrollArea className="max-h-[370px] h-full w-full">
+                <div className="w-full h-full flex flex-col gap-4">
+                  {data.map((gift) => (
+                    <div key={gift.id} className="w-full flex">
+                      <div className="w-1/3 shrink-0 aspect-square relative">
+                        <Image src={gift.imageUrl} alt={gift.name} fill className="object-cover object-center" />
                       </div>
 
-                      <Button
-                        onClick={() => removeGift(gift.id)}
-                        variant="ghost"
-                        className="w-fit h-fit p-0 flex items-center"
-                      >
-                        <Trash2 className="text-destructive" size={16} />
+                      <div className="w-full p-6 bg-secondary flex flex-col justify-between gap-6">
+                        <div className="w-full flex flex-col gap-2">
+                          <div className="w-full flex items-center gap-2">
+                            <div className="w-8 h-px bg-background" />
 
-                        <span className="text-base text-destructive uppercase">Remover</span>
-                      </Button>
+                            <h4 className="text-lg font-montserrat font-normal text-background uppercase line-clamp-1">
+                              {gift.name}
+                            </h4>
+                          </div>
+
+                          <h2 className="text-3xl font-montserrat font-normal text-background">
+                            {formatPrice(gift.price / 100)}
+                          </h2>
+                        </div>
+
+                        <Button
+                          onClick={() => removeGift(gift.id)}
+                          variant="ghost"
+                          className="w-fit h-fit p-0 flex items-center"
+                        >
+                          <Trash2 className="text-destructive" size={16} />
+
+                          <span className="text-base text-destructive uppercase">Remover</span>
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-            </ScrollArea>
+                  ))}
+                </div>
+              </ScrollArea>
 
-            <div className="w-full flex flex-col gap-9">
               <div className="w-full flex flex-col gap-4">
                 <div className="w-full h-px bg-primary/35" />
 
@@ -220,30 +222,34 @@ export function CartDialog({ width, openCart, giftsSelected, setGiftsSelected, s
                   <span className="font-montserrat text-xl font-light uppercase">{formatPrice(totalPrice / 100)}</span>
                 </div>
               </div>
-
-              <div className="w-full flex flex-col gap-4">
-                <Input className="w-full outline-input text-base" placeholder="NOME" />
-
-                <Textarea
-                  className="w-full outline-input resize-none !h-36 text-base"
-                  placeholder="DEIXE SUA MENSAGEM PARA OS NOIVOS"
-                />
-              </div>
             </div>
 
-            <div className="w-full grid grid-cols-2 gap-4">
-              <Button
-                onClick={() => setOpenCart(false)}
-                size="lg"
-                variant="outline"
-                className="uppercase font-light text-base"
-              >
-                Adicionar mais itens
-              </Button>
+            <div className="w-1/2 h-full flex flex-col justify-between gap-12">
+              <div className="w-full flex flex-col gap-9">
+                <div className="w-full flex flex-col gap-4">
+                  <Input className="w-full outline-input text-base" placeholder="NOME" />
 
-              <Button size="lg" className="uppercase font-light text-base">
-                Continuar compra
-              </Button>
+                  <Textarea
+                    className="w-full outline-input resize-none !h-36 text-base"
+                    placeholder="DEIXE SUA MENSAGEM PARA OS NOIVOS"
+                  />
+                </div>
+              </div>
+
+              <div className="w-full grid grid-rows-2 gap-4">
+                <Button
+                  onClick={() => setOpenCart(false)}
+                  size="lg"
+                  variant="outline"
+                  className="uppercase font-light text-base"
+                >
+                  Adicionar mais itens
+                </Button>
+
+                <Button size="lg" className="uppercase font-light text-base">
+                  Continuar compra
+                </Button>
+              </div>
             </div>
           </div>
         ) : (
