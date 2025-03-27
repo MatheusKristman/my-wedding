bb8d8cd3bd4e1b7067434a19bd17154715648ed3
MatheusKristman <kristman058@gmail.com>
2025-03-27 16:16:14 -0300

    added terms and privacy policy pages

---
+++ app/(website)/politica-de-privacidade/page.tsx
@@ -0,0 +1,138 @@
+const PrivacyPage = () => {
+  return (
+    <main className="w-full pt-12 pb-24 px-6 sm:py-24 sm:px-16 lg:container lg:mx-auto">
+      <h1 className="font-fonde text-4xl mb-12 sm:text-5xl lg:text-8xl">Política de privacidade</h1>
+
+      <div className="w-full flex flex-col gap-9">
+        <div className="w-full flex flex-col gap-4">
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            A sua privacidade é importante para nós. É política do Casamento M&G respeitar a sua privacidade em relação
+            a qualquer informação sua que possamos coletar no site Casamento M&G, e outros sites que possuímos e
+            operamos.
+          </p>
+
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço.
+            Faremo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que
+            estamos coletando e como será usado.
+          </p>
+
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando
+            armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem
+            como acesso, divulgação, cópia, uso ou modificação não autorizados.
+          </p>
+
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido
+            por lei.
+          </p>
+
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos
+            controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas
+            respectivas políticas de privacidade.
+          </p>
+
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos
+            fornecer alguns dos serviços desejados.
+          </p>
+
+          <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+            O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e
+            informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados pessoais e informações
+            pessoais, entre em contato conosco.
+          </p>
+
+          <ul className="w-full flex flex-col gap-4 list-decimal pl-4">
+            <li>
+              <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                O serviço Google AdSense que usamos para veicular publicidade usa um cookie DoubleClick para veicular
+                anúncios mais relevantes em toda a Web e limitar o número de vezes que um determinado anúncio é exibido
+                para você.
+              </p>
+            </li>
+
+            <li>
+              <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                Para mais informações sobre o Google AdSense, consulte as FAQs oficiais sobre privacidade do Google
+                AdSense.
+              </p>
+            </li>
+
+            <li>
+              <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                Utilizamos anúncios para compensar os custos de funcionamento deste site e fornecer financiamento para
+                futuros desenvolvimentos. Os cookies de publicidade comportamental usados ​​por este site foram
+                projetados para garantir que você forneça os anúncios mais relevantes sempre que possível, rastreando
+                anonimamente seus interesses e apresentando coisas semelhantes que possam ser do seu interesse.
+              </p>
+            </li>
+
+            <li>
+              <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                Vários parceiros anunciam em nosso nome e os cookies de rastreamento de afiliados simplesmente nos
+                permitem ver se nossos clientes acessaram o site através de um dos sites de nossos parceiros, para que
+                possamos creditá-los adequadamente e, quando aplicável, permitir que nossos parceiros afiliados ofereçam
+                qualquer promoção que pode fornecê-lo para fazer uma compra.
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">Compromisso do Usuário</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Casamento M&G oferece no
+              site e com caráter enunciativo, mas não limitativo:
+            </p>
+
+            <ul className="w-full flex flex-col gap-4 list-decimal pl-4">
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, 166bet ou azar, qualquer tipo
+                  de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Casamento M&G, de seus
+                  fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros
+                  sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">Mais informações</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza
+              se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos
+              que você usa em nosso site.
+            </p>
+
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Esta política é efetiva a partir de 27 de março de 2025 ás 17:37.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default PrivacyPage;
---
+++ app/(website)/termos-de-uso/page.tsx
@@ -0,0 +1,165 @@
+const TermsPage = () => {
+  return (
+    <main className="w-full pt-12 pb-24 px-6 sm:py-24 sm:px-16 lg:container lg:mx-auto">
+      <h1 className="font-fonde text-4xl mb-12 sm:text-5xl lg:text-8xl">Termos de uso</h1>
+
+      <div className="w-full flex flex-col gap-9">
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">1. Termos</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Ao acessar ao site Casamento M&G, concorda em cumprir estes termos de serviço, todas as leis e
+              regulamentos aplicáveis ​​e concorda que é responsável pelo cumprimento de todas as leis locais
+              aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou acessar este site. Os
+              materiais contidos neste site são protegidos pelas leis de direitos autorais e marcas comerciais
+              aplicáveis.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">2. Uso de Licença</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou software) no
+              site Casamento M&G , apenas para visualização transitória pessoal e não comercial. Esta é a concessão de
+              uma licença, não uma transferência de título e, sob esta licença, você não pode:
+            </p>
+
+            <ul className="w-full flex flex-col gap-4 list-decimal pl-4">
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  modificar ou copiar os materiais;
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial ou não
+                  comercial);
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  tentar descompilar ou fazer engenharia reversa de qualquer software contido no site Casamento M&G;
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  transferir os materiais para outra pessoa ou &apos;espelhe&apos; os materiais em qualquer outro
+                  servidor.
+                </p>
+              </li>
+            </ul>
+
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá ser
+              rescindida por Casamento M&G a qualquer momento. Ao encerrar a visualização desses materiais ou após o
+              término desta licença, você deve apagar todos os materiais baixados em sua posse, seja em formato
+              eletrônico ou impresso.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">3. Isenção de responsabilidade</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <ul className="w-full flex flex-col gap-4 list-decimal pl-4">
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  Os materiais no site da Casamento M&G são fornecidos &apos;como estão&apos;. Casamento M&G não oferece
+                  garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras garantias,
+                  incluindo, sem limitação, garantias implícitas ou condições de comercialização, adequação a um fim
+                  específico ou não violação de propriedade intelectual ou outra violação de direitos.
+                </p>
+              </li>
+
+              <li>
+                <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+                  Além disso, o Casamento M&G não garante ou faz qualquer representação relativa à precisão, aos
+                  resultados prováveis ​​ou à confiabilidade do uso dos materiais em seu site ou de outra forma
+                  relacionado a esses materiais ou em sites vinculados a este site.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">4. Limitações</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Em nenhum caso o Casamento M&G ou seus fornecedores serão responsáveis ​por quaisquer danos (incluindo,
+              sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos negócios) decorrentes do uso
+              ou da incapacidade de usar os materiais em Casamento M&G, mesmo que Casamento M&G ou um representante
+              autorizado da Casamento M&G tenha sido notificado oralmente ou por escrito da possibilidade de tais danos.
+              Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações de
+              responsabilidade por danos consequentes ou incidentais, essas limitações podem não se aplicar a você.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">5. Precisão dos materiais</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Os materiais exibidos no site da Casamento M&G podem incluir erros técnicos, tipográficos ou fotográficos.
+              Casamento M&G não garante que qualquer material em seu site seja preciso, completo ou atual. Casamento M&G
+              pode fazer alterações nos materiais contidos em seu site a qualquer momento, sem aviso prévio. No entanto,
+              Casamento M&G não se compromete a atualizar os materiais.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">6. Links</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              O Casamento M&G não analisou todos os sites vinculados ao seu site e não é responsável pelo conteúdo de
+              nenhum site vinculado. A inclusão de qualquer link não implica endosso por Casamento M&G do site. O uso de
+              qualquer site vinculado é por conta e risco do usuário.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">Modificações</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              O Casamento M&G pode revisar estes termos de serviço do site a qualquer momento, sem aviso prévio. Ao usar
+              este site, você concorda em ficar vinculado à versão atual desses termos de serviço.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2 className="font-montserrat text-2xl leading-relaxed">Lei aplicável</h2>
+
+          <div className="w-full flex flex-col gap-4">
+            <p className="font-montserrat text-lg text-foreground/70 leading-relaxed sm:text-xl">
+              Estes termos e condições são regidos e interpretados de acordo com as leis do Casamento M&G e você se
+              submete irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou localidade.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default TermsPage;
